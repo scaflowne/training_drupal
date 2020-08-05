9d1ef444c4af1f9114e5e4c9743bf6d920a3b72b
Alexander Prada <aprada@weknowinc.com>
2020-08-05 15:58:04 -0500

    Created Bits Pages Links Controller

---
+++ web/modules/custom/bits_pages/src/Controller/BitsPagesLinksController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\bits_pages\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\RendererInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Class BitsPagesLinksController.
+ */
+class BitsPagesLinksController extends ControllerBase {
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a BitsPagesLinksController object.
+   *
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   */
+  public function __construct(RendererInterface $renderer) {
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('renderer'),
+    );
+  }
+
+  /**
+   * Links.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function links() {
+
+    $items = [];
+    $items[] = Link::fromTextAndUrl('Administración de bloques',
+      Url::fromUserInput('/admin/structure/block'));
+    $items[] = Link::fromTextAndUrl('Administración de contenidos',
+      Url::fromUserInput('/admin/content'));
+    $items[] = Link::fromTextAndUrl('Administración de usuarios',
+      Url::fromUserInput('/admin/people'));
+    $items[] = Link::fromTextAndUrl('Enlace a la portada del sitio',
+      Url::fromUri('internal:/'));
+    $items[] = Link::createFromRoute('Enlace al nodo con id 1',
+      'entity.node.canonical', ['node' => 1]);
+    $items[] = Link::fromTextAndUrl('Enlace a la edición del nodo con id 1',
+      Url::fromUri('internal:/node/1/edit'));
+    $items[] = Link::fromTextAndUrl('Enlace externo a www.google.com',
+      Url::fromUri('http://www.google.com', ['attributes' => ['target' => '_blank']]));
+
+    $output = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+      '#title' => 'My Menu',
+      '#items' => $items,
+      '#attributes' => ['class' => 'myclass'],
+      '#wrapper_attributes' => ['class' => 'my_list_container'],
+    ];
+
+    return [
+      '#type' => 'markup',
+      '#markup' =>  $this->renderer->render($output)
+    ];
+  }
+
+}
